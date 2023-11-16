2f567c3fabcf4fefdc1ebd0a287b4a97a154f230
Mohit Ahlawat <mohitahlawat223@gmail.com>
2023-11-16 13:36:58 +0530

    Login form backend

---
+++ db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+module.exports = () => {
+
+    try {
+        mongoose.connect(process.env.DB);
+        console.log("Connected to DataBase Successfully");
+    } catch (error) {
+        console.log(error);
+    }
+}
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+require('dotenv').config();
+const app = express();  
+const cors = require("cors");
+const DBconnection = require("./db");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth"); 
+
+
+//DataBase Conncetion
+DBconnection();
+
+
+
+//MiddleWares
+app.use(express.json());
+app.use(cors());
+
+
+//Routes
+app.use("/users", userRoutes);
+app.use("/auth", authRoutes); 
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
